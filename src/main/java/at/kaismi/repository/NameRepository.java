168121f4a5d828602e570b1ca6748929a81d7962
Michael Kaiser <kaiser_michael@gmx.at>
2018-02-12 11:39:50 +0100

    use constructor based dependency injection

--- src/main/java/at/kaismi/repository/NameRepository.java
+++ src/main/java/at/kaismi/repository/NameRepository.java
@@ -1,11 +1,13 @@
 package at.kaismi.repository;
 
+import com.google.common.collect.Sets;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
-@Repository public class NameRepository {
+@Repository
+public class NameRepository {
 
-    public static Set<String> names;
+    public static Set<String> names = Sets.newHashSet();
 
 }
--- src/main/java/at/kaismi/repository/TaskRepository.java
+++ src/main/java/at/kaismi/repository/TaskRepository.java
@@ -1,11 +1,13 @@
 package at.kaismi.repository;
 
 import at.kaismi.domain.Task;
+import com.google.common.collect.Sets;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
-@Repository public class TaskRepository {
+@Repository
+public class TaskRepository {
 
-    public static Set<Task> tasks;
+    public static Set<Task> tasks = Sets.newHashSet();
 }
